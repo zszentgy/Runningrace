9fae5b39ffd05b6cfc28576271896a3bb517e318
Zoltán Szentgyörgyi <zszentgy@gmail.com>
2024-04-22 18:11:39 +0200

    spring boot init

---
+++ src/main/java/com/runningrace/RunningraceApplication.java
@@ -0,0 +1,13 @@
+package com.runningrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/runningrace/RunningraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runningrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
