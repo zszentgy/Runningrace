d1c45f41f6a24367d8b37612a4443d5570eadf05
Zoltán Szentgyörgyi <zszentgy@gmail.com>
2024-04-22 18:58:20 +0200

    add getAllRunners

--- src/main/java/com/runningrace/controller/RunnerController.java
+++ src/main/java/com/runningrace/controller/RunnerController.java
@@ -1,9 +1,21 @@
 package com.runningrace.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.runningrace.model.Runner;
+import com.runningrace.repository.RunnerRepository;
 
 @RestController
-@RequestMapping("/api/runners")
+@RequestMapping("/api")
 public class RunnerController {
+    @Autowired
+    private RunnerRepository runnerRepository;
+
+    // GET: List all runners
+    @GetMapping("/getRunners")
+    public List<Runner> getAllRunners() {
+        return runnerRepository.findAll();
+    }
 
 }
